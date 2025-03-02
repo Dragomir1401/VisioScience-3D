3687d248b3c5db894555af6444d8a7520cddb295
Dragomir Andrei <andreidragomir1401@stud.acs.upb.ro>
2025-03-02 16:55:53 +0200

    mesh shows

--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { Html } from '@react-three/drei'
+import React from "react";
+import { Html } from "@react-three/drei";
 
 const Loader = () => {
   return (
     <Html>
-        <div className='w-full h-screen flex items-center justify-center'>
-            <div className='w-20 h-20 border-opacity-20 border-orange-500 border-8 rounded-full animate-spin'></div>
-        </div>
+      <div className="flex items-center justify-center">
+        <div className="w-20 h-20 border-opacity-20 border-orange-500 border-2 rounded-full animate-spin"></div>
+      </div>
     </Html>
-  )
-}
+  );
+};
 
-export default Loader
+export default Loader;
--- frontend/src/pages/Acasa.jsx
+++ frontend/src/pages/Acasa.jsx
@@ -1,25 +1,52 @@
-import React, { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import Loader from '../components/Loader'
-import Island from '../models/Island'
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
 
 const Acasa = () => {
+  const PositionIslandInCanvas = () => {
+    let canvasScale = null;
+    let canvasPosition = [0, -5.95, -46];
+    let canvasRotation = [0.05, 1.32, 0];
+
+    if (window.innerWidth < 768) {
+      canvasScale = [0.9, 0.9, 0.9];
+    } else {
+      canvasScale = [1, 1, 1];
+    }
+
+    return [canvasScale, canvasPosition, canvasRotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    PositionIslandInCanvas();
+
   return (
-    <section className='w-full h-screen relative flex items-center justify-center'>
-        <div className='absolute top-28 left-0 right-0 z-0 flex items-center justify-center'>
-            POPUP
-        </div>
+    <section className="w-full h-screen relative flex items-center justify-center">
+      <div className="absolute top-28 left-0 right-0 z-0 flex items-center justify-center">
+        {/* POPUP */}
+      </div>
 
-        <Canvas 
-            className='w-full h-screen bg-transparent'
-            camera={{ near: 0.1, far: 1000, position: [0, 0, 5] }}>
-            <Suspense fallback={<Loader />}>
-                {/* <Island /> */}
-            </Suspense>
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000, position: [0, 0, 5] }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
 
-        </Canvas>
+          <Island
+            scale={islandScale}
+            position={islandPosition}
+            rotation={islandRotation}
+          />
+        </Suspense>
+      </Canvas>
     </section>
-  )
-}
+  );
+};
 
-export default Acasa
+export default Acasa;
