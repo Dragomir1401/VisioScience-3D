398549da15965faacd96ef58dc4f7df56ab36fe0
Dragomir Andrei <andreidragomir1401@stud.acs.upb.ro>
2025-03-28 19:11:11 +0200

    same issue

--- backend/formulas_service/main.go
+++ backend/formulas_service/main.go
@@ -3,16 +3,17 @@ package main
 import (
     "log"
     "net/http"
-    "formulas_service/handlers"
 
+    "formulas_service/handlers"
     "github.com/gorilla/mux"
 )
 
 func main() {
     r := mux.NewRouter()
 
-	r.HandleFunc("/formulas/{shape}", handlers.GetFormulasByShape).Methods("GET")
-	r.HandleFunc("/formulas/{shape}", handlers.CreateFormula).Methods("POST")
+    r.HandleFunc("/formulas/{shape}", handlers.GetFormulas).Methods("GET")
+
+    r.HandleFunc("/formulas/{shape}", handlers.CreateFormula).Methods("POST")
 
     log.Println("Server running on port 8080...")
     if err := http.ListenAndServe(":8080", r); err != nil {
