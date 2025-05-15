16e69f44709bfb1f2fa98239caa3b38948be267f
Dragomir Andrei <andreidragomir1401@stud.acs.upb.ro>
2025-05-15 23:48:15 +0300

    [BE] added sketch of monitoring service

---
+++ backend/monitoring-service/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	gorillaHandlers "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	r := mux.NewRouter()
+
+	corsObj := gorillaHandlers.CORS(
+		gorillaHandlers.AllowedOrigins([]string{"*"}),
+		gorillaHandlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		gorillaHandlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+	)
+
+	fs := http.FileServer(http.Dir("/usr/share/grafana"))
+	r.PathPrefix("/grafana/").Handler(http.StripPrefix("/grafana/", fs))
+
+	r.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	}).Methods("GET")
+
+	log.Println("Monitoring service running on :8082")
+	if err := http.ListenAndServe(":8082", corsObj(r)); err != nil {
+		log.Fatal(err)
+	}
+}
