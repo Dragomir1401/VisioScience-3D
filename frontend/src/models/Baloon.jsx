46dd2ae3b11c71a24556a84f8ab231b826e08ae7
Dragomir Andrei <andreidragomir1401@stud.acs.upb.ro>
2025-06-30 17:08:57 +0300

    added ballon click

--- frontend/src/models/Baloon.jsx
+++ frontend/src/models/Baloon.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { a, useSpring } from "@react-spring/three";
 import baloonScene from "../assets/3d/baloon.glb";
@@ -6,9 +6,11 @@ import baloonScene from "../assets/3d/baloon.glb";
 const Baloon = (props) => {
   const { scene } = useGLTF(baloonScene);
   const [animationState, setAnimationState] = useState("idle");
+  const animationTimeout = useRef();
 
-  useEffect(() => {
-    const interval = setInterval(() => {
+  const scheduleRandomAnimation = () => {
+    clearTimeout(animationTimeout.current);
+    animationTimeout.current = setTimeout(() => {
       const states = [
         "idle",
         "swayLeft",
@@ -20,17 +22,24 @@ const Baloon = (props) => {
       ];
       const randomState = states[Math.floor(Math.random() * states.length)];
       setAnimationState(randomState);
-    }, 2000);
+    }, 3000 + Math.random() * 2000);
+  };
 
-    return () => clearInterval(interval);
-  }, []);
+  useEffect(() => {
+    if (animationState !== "jump") {
+      scheduleRandomAnimation();
+    }
+    return () => clearTimeout(animationTimeout.current);
+  }, [animationState]);
 
-  const { position, rotation } = useSpring({
+  const { position, rotation, scale } = useSpring({
     position:
       animationState === "floatUp"
         ? [props.position[0], props.position[1] + 0.3, props.position[2]]
         : animationState === "floatDown"
         ? [props.position[0], props.position[1] - 0.3, props.position[2]]
+        : animationState === "jump"
+        ? [props.position[0], props.position[1] + 0.6, props.position[2]]
         : props.position,
     rotation:
       animationState === "swayLeft"
@@ -41,12 +50,38 @@ const Baloon = (props) => {
         ? [props.rotation[0] + 0.1, props.rotation[1], props.rotation[2]]
         : animationState === "tiltBackward"
         ? [props.rotation[0] - 0.1, props.rotation[1], props.rotation[2]]
+        : animationState === "jump"
+        ? [props.rotation[0] + 0.1, props.rotation[1], props.rotation[2] - 0.2]
         : props.rotation,
-    config: { mass: 1, tension: 100, friction: 85, duration: 2000 },
+    scale:
+      animationState === "jump"
+        ? [props.scale[0] * 1.15, props.scale[1] * 1.15, props.scale[2] * 1.15]
+        : props.scale,
+    config:
+      animationState === "jump"
+        ? { mass: 1, tension: 500, friction: 30 }
+        : { mass: 2, tension: 100, friction: 85, duration: 2500 },
+    onRest: () => {
+      if (animationState === "jump") {
+        setAnimationState("idle");
+      }
+    },
   });
 
+  const handleClick = (e) => {
+    e.stopPropagation();
+    if (animationState !== "jump") {
+      setAnimationState("jump");
+    }
+  };
+
   return (
-    <a.mesh position={position} rotation={rotation} scale={props.scale}>
+    <a.mesh
+      position={position}
+      rotation={rotation}
+      scale={scale}
+      onClick={handleClick}
+    >
       <primitive object={scene} />
     </a.mesh>
   );
